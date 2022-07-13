2946d5ff37ce0ed1b37fbcb8cc2158bed989da7e
Felix Friedrich <ge42fih@mytum.de>
2022-07-13 13:22:43 +0200

    second try

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,7 +13,23 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "felix";
-        } else { // TODO extend the programm here
+        } else if (query.contains("plus")) {
+            //what is 9 plus 3
+            String[] wordList = query.split(" ");
+            int sum = 0;
+
+            for (String word : wordList){
+                try {
+                    int intOfWord = Integer.parseInt(word);
+                    sum+= intOfWord;
+                }
+                catch (Exception e){
+                    //
+                }
+            }
+            return String.valueOf(sum);
+        }
+        else { // TODO extend the programm here
             //hello world
 
 
